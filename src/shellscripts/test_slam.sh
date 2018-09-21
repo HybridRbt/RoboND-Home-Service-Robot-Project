a645e9543f01acd3d2124480a296d279eeed29bb
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2018-09-20 22:05:07 -0500

    wrong cd cmd

--- src/shellscripts/test_slam.sh
+++ src/shellscripts/test_slam.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-xterm -e " cd ../../..; cd turtlebot_simulator/turtlebot_gazebo/launch/; roslaunch turtlebot_world.launch " &
+xterm -e " cd ..; cd turtlebot_simulator/turtlebot_gazebo/launch/; roslaunch turtlebot_world.launch " &
 sleep 1
 xterm -e " roslaunch gmapping_demo.launch " &
-# sleep 1
-# xterm -e " cd ../../..; cd turtlebot_interactions/turtlebot_rivz_launchers/launch/; roslaunch view_navigation.launch " &
-# sleep 1
-# xterm -e " cd ..; cd turtlebot/turtlebot_teleop/launch/; roslaunch keyboard_teleop.launch "
+sleep 1
+xterm -e " cd ../../..; cd turtlebot_interactions/turtlebot_rivz_launchers/launch/; roslaunch view_navigation.launch " &
+sleep 1
+xterm -e " cd ../../..; cd turtlebot/turtlebot_teleop/launch/; roslaunch keyboard_teleop.launch "
