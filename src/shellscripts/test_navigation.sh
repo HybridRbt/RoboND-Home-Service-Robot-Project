e17a5295076f842ed2b0bc40ff2ba0ebfa0ee542
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2018-09-23 14:35:26 -0500

    modify script to use wood u world

--- src/shellscripts/test_navigation.sh
+++ src/shellscripts/test_navigation.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 xterm -e " cd ..; cd launch/; roslaunch turtlebot_wooduworld.launch " &
 sleep 5
-xterm -e " cd ..; cd turtlebot_simulator/turtlebot_gazebo/launch/; roslaunch amcl_demo.launch " &
+xterm -e " cd ..; cd launch/; roslaunch amcl_woodu.launch " &
 sleep 5
-xterm -e " cd ..; cd turtlebot_interactions/turtlebot_rviz_launchers/launch/; roslaunch view_navigation.launch " 
+xterm -e " cd ..; cd turtlebot_interactions/turtlebot_rviz_launchers/launch/; roslaunch view_navigation.launch "
