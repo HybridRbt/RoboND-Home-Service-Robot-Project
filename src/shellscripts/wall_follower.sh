7ab4d2bd127fdecb73bb00cf49155c9644de2db2
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2018-09-22 22:38:49 -0500

    Revert "result is worse; use default launch file"
    This reverts commit d33e9527e5c30c4d89094e5c2aa7df8ca970b8b1.

--- src/shellscripts/wall_follower.sh
+++ src/shellscripts/wall_follower.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 xterm -e " cd ..; cd launch/; roslaunch turtlebot_wooduworld.launch " &
 sleep 5
-xterm -e " cd ..; cd turtlebot_simulator/turtlebot_gazebo/launch/; roslaunch gmapping_demo.launch " &
+xterm -e " cd ..; cd wall_follower/launch/; roslaunch gmapping_woodu.launch " &
 sleep 5
 xterm -e " cd ..; cd turtlebot_interactions/turtlebot_rviz_launchers/launch/; roslaunch view_navigation.launch " &
 sleep 5
