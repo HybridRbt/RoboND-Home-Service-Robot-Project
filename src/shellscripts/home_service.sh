1aecf3763348c7cea7a0cebff9c07fdf27dbbe88
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2018-09-25 21:36:48 -0500

    new script

---
+++ src/shellscripts/home_service.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+xterm -e " cd ..; cd launch/; roslaunch turtlebot_wooduworld.launch " &
+sleep 5
+xterm -e " cd ..; cd launch/; roslaunch amcl_woodu.launch " &
+sleep 5
+xterm -e " cd ..; cd pick_objects/launch/; roslaunch home_service.launch " &
+sleep 5
+xterm -e " cd ..; cd launch/; roslaunch pick_objects.launch " &
+sleep 5
+xterm -e " cd ..; cd launch/; roslaunch add_markers.launch "
