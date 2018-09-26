f71b0cf934b5b723d4d6fb9276b34782cb3c99cf
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2018-09-25 22:37:36 -0500

    rearrange launch sequence

--- src/shellscripts/home_service.sh
+++ src/shellscripts/home_service.sh
@@ -3,8 +3,8 @@ xterm -e " cd ..; cd launch/; roslaunch turtlebot_wooduworld.launch " &
 sleep 5
 xterm -e " cd ..; cd launch/; roslaunch amcl_woodu.launch " &
 sleep 5
+xterm -e " cd ..; cd launch/; roslaunch add_markers.launch " &
+sleep 5
 xterm -e " cd ..; cd launch/; roslaunch home_service.launch " &
 sleep 5
-xterm -e " cd ..; cd launch/; roslaunch pick_objects.launch " &
-sleep 5
-xterm -e " cd ..; cd launch/; roslaunch add_markers.launch "
+xterm -e " cd ..; cd launch/; roslaunch pick_objects.launch "
